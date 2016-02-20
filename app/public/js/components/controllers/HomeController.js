8b3c5a01cfae879b13abf352c09244d9ffbb0f39
John's Dashboard Extraordinaire <sax1johno@gmail.com>
2016-02-20 19:55:28 +0000

    Got the interface working and connecting to the services via rest

--- app/public/js/components/controllers/HomeController.js
+++ app/public/js/components/controllers/HomeController.js
@@ -10,10 +10,82 @@ angular.module('isavewater_simulator')
     //     $scope.loaded = true;
     //     $mdToast.show($mdToast.simple().textContent(res.error));
     // });
-    console.log(settings.getConfig());
     $scope.areas = settings.getConfig().areas;
-    console.log("Config = ", settings.getConfig().areas);
+    $scope.loaded = true;
+    angular.forEach($scope.areas, function(area) {
+        // Set defaults for the areas.
+        $scope.data = {};
+        $scope.data.area = {};
+        $scope.data.area[area.name] = {
+            flow: {
+                enabled: false,
+                rate: 0
+            }
+        }
+    });
+    
     $scope.overcurrent = function(area, state) {
-        $mdToast.show($mdToast.simple().textContent("Should have set overcurrent for area " + area.name + " to " + state));
+        console.log("Called overcurrent method!");
+        var activate = state ? "activate" : "deactivate";
+        var url = "/api/v1/simulator/" + activate;
+        $http.post(url, {
+            type: "overcurrent",
+            area: area
+        }).then(function(res){ 
+            console.log("Posted to service for overcurrent", res);
+            $mdToast.show($mdToast.simple().textContent("Should have set overcurrent for area " + area.name + " to " + state));
+        }, function(res) {
+            $mdToast.show($mdToast.simple().textContent(res.error));
+            state = !state;
+        });
     }
+
+    $scope.flow = function(area, state) {
+        console.log("Called flow method!");
+        var activate = state ? "activate" : "deactivate";
+        var url = "/api/v1/simulator/" + activate;
+        $http.post(url, {
+            type: "flow",
+            area: area
+        }).then(function(res){ 
+            console.log("Posted to service for flow", res);
+            $mdToast.show($mdToast.simple().textContent("Should have set flow for area " + area.name + " to " + state));
+        }, function(res) {
+            $mdToast.show($mdToast.simple().textContent("Unable to set flow to state", state));
+            state = !state;
+        });
+    }
+    
+    $scope.zoneFn = function(zone, state) {
+        console.log("Called flow method!");
+        var activate = state ? "activate" : "deactivate";
+        var url = "/api/v1/simulator/" + activate;
+        $http.post(url, {
+            type: "zone",
+            zone: zone
+        }).then(function(res){ 
+            console.log("Posted to service for zone", res);
+            $mdToast.show($mdToast.simple().textContent("Should have set " + zone.name + " to " + state));
+        }, function(res) {
+            $mdToast.show($mdToast.simple().textContent("Unable to set " + zone.name + " to state", state));
+            state = !state;
+        });
+    }
+    
+    $scope.flowRate = function(area, rate) {
+        console.log("Called flow method!");
+        var activate = "activate";
+        var url = "/api/v1/simulator/" + activate;
+        $http.post(url, {
+            type: "flow",
+            area: area,
+            rate: rate
+        }).then(function(res){ 
+            console.log("Posted to service for flow", res);
+            $mdToast.show($mdToast.simple().textContent("Should have set flow rate for area " + area.name + " to " + rate));
+        }, function(res) {
+            $mdToast.show($mdToast.simple().textContent("Unable to set flow rate"));
+        });
+    }
+    
 });
